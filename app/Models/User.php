8973d7436a828f634b1941fb5db7c9d0318fd268
momonga <kienlt.dev@gmail.com>
2020-01-01 12:00:00 +0700

    Initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+
+
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
